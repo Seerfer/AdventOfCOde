2845c1523020da07540fdd9f3d0b4512f74be4ba
Patryk Ostrowski <patrykost1998@gmail.com>
2022-12-06 17:23:43 +0100

    Day 4 - get_full_list_of_sections test green

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,3 +1,5 @@
 from typing import List
-def get_full_list_of_sections(range: str) -> List[int]:
-    pass
+def get_full_list_of_sections(given_range: str) -> List[int]:
+    splited = given_range.split("-")
+    start, end = int(splited[0]), int(splited[1])
+    return list(range(start, end+1))
