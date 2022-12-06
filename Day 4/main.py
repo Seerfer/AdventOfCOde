2b34f9aa9622dfb265ec7092f6f68e273f77ccaa
Patryk Ostrowski <patrykost1998@gmail.com>
2022-12-06 17:32:08 +0100

    Day 4 - check_if_contains test green

--- Day 4/main.py
+++ Day 4/main.py
@@ -6,4 +6,5 @@ def get_full_list_of_sections(given_range: str) -> List[int]:
     return list(range(start, end+1))
 
 def check_if_contains(list1: list, list2: list) -> bool:
-    pass
+    set1, set2 = set(list1), set(list2)
+    return set1.union(set2) == set1 or set2.union(set1) == set2
