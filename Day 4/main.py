78eb4e1c1e2f30fcb2dd6dba55fea608d16a380c
Patryk Ostrowski <patrykost1998@gmail.com>
2022-12-06 17:30:08 +0100

    Day 4 - check_if_contains test red

--- Day 4/main.py
+++ Day 4/main.py
@@ -1,5 +1,9 @@
 from typing import List
+
 def get_full_list_of_sections(given_range: str) -> List[int]:
     splited = given_range.split("-")
     start, end = int(splited[0]), int(splited[1])
     return list(range(start, end+1))
+
+def check_if_contains(list1: list, list2: list) -> bool:
+    pass
--- Day 4/tests.py
+++ Day 4/tests.py
@@ -1,6 +1,6 @@
 import unittest
 
-from main import get_full_list_of_sections
+from main import get_full_list_of_sections, check_if_contains
 
 class Test(unittest.TestCase):
     def test_get_full_list_of_sections(self):
@@ -9,7 +9,19 @@ class Test(unittest.TestCase):
         out = get_full_list_of_sections(input)
         self.assertEqual(expected, out)
 
+    def test_check_if_contains_true(self):
+        list1 = [6]
+        list2 = [4,5,6]
+        expected = True
+        out = check_if_contains(list1, list2)
+        self.assertEqual(expected, out)
 
+    def test_check_if_contains_false(self):
+        list1 = [2,3]
+        list2 = [4,5]
+        expected = False
+        out = check_if_contains(list1, list2)
+        self.assertEqual(expected, out)
 
 if __name__ == '__main__':
     unittest.main()
