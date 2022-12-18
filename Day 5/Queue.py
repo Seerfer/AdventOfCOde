84846c7b83b77d67b1e0e118d3d57e74fccd4a75
Patryk Ostrowski <patrykost1998@gmail.com>
2022-12-18 11:35:17 +0100

    Implementation (tests green)

--- Day 5/Queue.py
+++ Day 5/Queue.py
@@ -1,22 +1,21 @@
 
-
 class Queue:
 
     def __init__(self, starting_elements: list = []):
-        self.__stack = starting_elements
+        self.__queue = starting_elements
 
     def push(self, element) -> None:
-        pass
+        self.__queue.append(element)
 
     def is_empty(self) -> bool:
-        pass
+        return bool(not self.__queue)
 
-    def pop(self) -> None:
-        pass
+    def pop(self):
+        return self.__queue.pop()
 
     def check_top(self):
-        pass
+        return self.__queue[-1]
 
     @property
     def return_queue(self):
-        pass
+        return self.__queue
