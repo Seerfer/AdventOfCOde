9cd1f5474fab80fca6addd4039e7d35d26511824
Patryk Ostrowski <patrykost1998@gmail.com>
2022-12-18 11:27:54 +0100

    Queue tests red

---
+++ Day 5/Queue.py
@@ -0,0 +1,22 @@
+
+
+class Queue:
+
+    def __init__(self, starting_elements: list = []):
+        self.__stack = starting_elements
+
+    def push(self, element) -> None:
+        pass
+
+    def is_empty(self) -> bool:
+        pass
+
+    def pop(self) -> None:
+        pass
+
+    def check_top(self):
+        pass
+
+    @property
+    def return_queue(self):
+        pass
---
+++ Day 5/tests.py
@@ -0,0 +1,47 @@
+import unittest
+
+from Queue import Queue
+
+class Test_stack(unittest.TestCase):
+
+    def test_queue_return(self):
+        el = [1, 2, 3, 4, 5]
+        q = Queue(el)
+        out = q.return_queue
+        self.assertEqual(out, el)
+
+    def test_queue_push(self):
+        q = Queue([1,2,3,4,5])
+        expected = [1,2,3,4,5,6]
+        q.push(6)
+        out = q.return_queue
+        self.assertEqual(out, expected)
+
+    def test_queue_pop(self):
+        q = Queue([1,2,3,4,5])
+        expected = [1,2,3,4]
+        q.pop()
+        out = q.return_queue
+        self.assertEqual(out, expected)
+
+    def test_queue_top(self):
+        q = Queue([1,2,3,4,5])
+        expected = 5
+        out = q.check_top()
+        self.assertEqual(out, expected)
+
+    def test_is_empty_false(self):
+        q = Queue([1,2,3,4,5])
+        expected = False
+        out = q.is_empty()
+        self.assertEqual(out, expected)
+
+    def test_is_empty_true(self):
+        q = Queue([])
+        expected = True
+        out = q.is_empty()
+        self.assertEqual(out, expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
