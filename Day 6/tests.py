6fa6b34555f86a9c568d24ffedd0001d7cec7a77
Patryk Ostrowski <patrykost1998@gmail.com>
2023-02-23 20:02:39 +0100

    take_next_n tests defined

---
+++ Day 6/tests.py
@@ -0,0 +1,46 @@
+import unittest
+
+from main import take_next_n_el, list_count_distinct
+
+
+class Test_take_next_n_el(unittest.TestCase):
+    def test_take_next_n_el_start_begining(self):
+        input_data = [1, 2, 3, 4, 5, 6]
+        starting = 0
+        n = 3
+        expected = [2, 3, 4]
+        out = take_next_n_el(input_data, starting, n)
+        self.assertEqual(expected, out)
+
+    def test_take_next_n_el_start_middle(self):
+        input_data = [1, 2, 3, 4, 5, 6]
+        starting = 2
+        n = 2
+        expected = [4, 5]
+        out = take_next_n_el(input_data, starting, n)
+        self.assertEqual(expected, out)
+
+    def test_take_next_n_negative_n(self):
+        input_data = [1, 2, 3, 4, 5, 6]
+        starting = 0
+        n = -3
+        with self.assertRaises(ValueError):
+            out = take_next_n_el(input_data, starting, n)
+
+    def test_take_next_n_index_error(self):
+        input_data = [1, 2, 3, 4, 5, 6]
+        starting = 0
+        n = 8
+        with self.assertRaises(IndexError):
+            out = take_next_n_el(input_data, starting, n)
+
+    def test_take_next_n_negative_starting(self):
+        input_data = [1, 2, 3, 4, 5, 6]
+        starting = -2
+        n = 8
+        with self.assertRaises(ValueError):
+            out = take_next_n_el(input_data, starting, n)
+
+
+if __name__ == "__main__":
+    unittest.main()
