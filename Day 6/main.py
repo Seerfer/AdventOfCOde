b2fcbff024f06069337dbce4b7f52aaa26afd0d7
Patryk Ostrowski <patrykost1998@gmail.com>
2023-02-23 20:11:46 +0100

    take_next_n_el implemented

--- Day 6/main.py
+++ Day 6/main.py
@@ -1,5 +1,11 @@
 def take_next_n_el(collection: list, starting_index: int, n: int) -> list:
-    pass
+    if n < 0:
+        raise ValueError("n must be positive")
+    if starting_index < 0:
+        raise ValueError("starting_index must be positive")
+    if starting_index + n + 1 > len(collection):
+        raise IndexError("Not enough element in the list to return")
+    return collection[starting_index + 1 : starting_index + 1 + n]
 
 
 def list_count_distinct(collection: list) -> int:
