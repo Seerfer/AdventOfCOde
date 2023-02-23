ba851a87663da4ae7c77ebad3a6fd02608d6b4f2
Patryk Ostrowski <patrykost1998@gmail.com>
2023-02-23 19:50:55 +0100

    functions definitions

---
+++ Day 6/main.py
@@ -0,0 +1,12 @@
+def take_next_n_el(collection: list, starting_index: int, n: int) -> list:
+    pass
+
+
+def list_count_distinct(collection: list) -> int:
+    pass
+
+
+if __name__ == "__main__":
+    result = 0
+    with open("input", "r") as f:
+        input_data = f.readline()
