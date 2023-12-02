3e9af7737e7e772a111aeb671d27fa174af87d66
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-02 11:02:57 +0100

    Part 2 finished

--- 2023/Day 1/main.py
+++ 2023/Day 1/main.py
@@ -1,19 +1,5 @@
 from typing import Optional, List
 
-def find_first_digit(s: str) -> Optional[int]:
-    for char in s:
-        if char.isdigit():
-            return int(char)
-    return None
-
-
-def find_last_digit(s: str) -> Optional[int]:
-    s_reversed = s[::-1]
-    for char in s_reversed:
-        if char.isdigit():
-            return int(char)
-    return None
-
 
 def create_number_from_digits(*args: str) -> int:
     s = ""
@@ -23,12 +9,6 @@ def create_number_from_digits(*args: str) -> int:
         else:
             raise ValueError(f"Value {a} is not a digit")
     return int(s)
-
-
-def replace_sub_str(base_str: str, start_i: int, end_i: int, str_to_replace: str) -> str:
-    if end_i > len(base_str)-1:
-        raise ValueError("End index out of range")
-    return base_str[:start_i]+str_to_replace+base_str[end_i+1:]
 
 
 def find_first_value(s: str, values: List[str]) -> Optional[str]:
@@ -56,43 +36,24 @@ def find_last_value(s: str, values: List[str]) -> Optional[str]:
     else:
         return None
 
-def rreplace(s, old, new, occurrence):
-    li = s.rsplit(old, occurrence)
-    return new.join(li)
-
-
-def replace_first_and_last_spelled_num_with_dig(s: str) -> str:
-    replace_dic = {
-        "one": 1,
-        "two": 2,
-        "three": 3,
-        "four": 4,
-        "five": 5,
-        "six": 6,
-        "seven": 7,
-        "eight": 8,
-        "nine": 9
+def transform_to_digit(s: str) -> Optional[str]:
+    digits_dict = {
+        "one":'1', "two":'2', "three":'3', "four":'4', "five":'5', "six":'6', "seven":'7', "eight":'8', "nine":'9'
     }
-    values = list(replace_dic.keys())
-
-    value_to_replace = find_first_value(s, values)
-    if value_to_replace is not None:
-        s = s.replace(value_to_replace, str(replace_dic[value_to_replace]), 1)
-
-    value_to_replace = find_last_value(s, values)
-    if value_to_replace is not None:
-        s = rreplace(s, value_to_replace, str(replace_dic[value_to_replace]), 1)
-
-    return s
-
+    return digits_dict.get(s, None)
 
 if __name__ == "__main__":
     nums = []
+    values_to_find_part1 = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
+    values_to_find_part2 = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
     with open("input", "r") as f:
         while line := f.readline().strip():
-            line_converted = replace_first_and_last_spelled_num_with_dig(line)
-            first_dig = find_first_digit(line_converted)
-            last_dig = find_last_digit(line_converted)
+            first_dig = find_first_value(line, values_to_find_part2)
+            if not first_dig.isdigit():
+                first_dig=transform_to_digit(first_dig)
+            last_dig = find_last_value(line, values_to_find_part2)
+            if not last_dig.isdigit():
+                last_dig=transform_to_digit(last_dig)
             num = create_number_from_digits(str(first_dig), str(last_dig))
             nums.append(num)
     print(sum(nums))
--- 2023/Day 1/tests.py
+++ 2023/Day 1/tests.py
@@ -1,58 +1,7 @@
 import unittest
 
-from main import find_first_digit, find_last_digit, create_number_from_digits, replace_first_and_last_spelled_num_with_dig, find_first_value, find_last_value
+from main import create_number_from_digits, find_first_value, find_last_value, transform_to_digit
 
-
-class Test_find_first_digit(unittest.TestCase):
-    def test_find_first_digit_digit_at_start(self):
-        input = "1dfrgv"
-        expected = 1
-        out = find_first_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_first_digit_in_the_middle(self):
-        input = "df5rgv"
-        expected = 5
-        out = find_first_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_first_no_digit(self):
-        input = "dfrgv"
-        expected = None
-        out = find_first_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_first_multiple_digits(self):
-        input = "d2fr4gv"
-        expected = 2
-        out = find_first_digit(input)
-        self.assertEqual(expected, out)
-
-
-class Test_find_last_digit(unittest.TestCase):
-    def test_find_last_digit_digit_at_end(self):
-        input = "dfrgv3"
-        expected = 3
-        out = find_last_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_last_digit_in_the_middle(self):
-        input = "df5rgv"
-        expected = 5
-        out = find_last_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_last_no_digit(self):
-        input = "dfrgv"
-        expected = None
-        out = find_last_digit(input)
-        self.assertEqual(expected, out)
-
-    def test_find_last_multiple_digits(self):
-        input = "d2fr4gv"
-        expected = 4
-        out = find_last_digit(input)
-        self.assertEqual(expected, out)
 
 
 class Test_create_number_from_digits(unittest.TestCase):
@@ -73,27 +22,6 @@ class Test_create_number_from_digits(unittest.TestCase):
     def test_create_number_from_digits_two_digit_num(self):
         with self.assertRaises(ValueError):
             create_number_from_digits("13", "2", "e", "3")
-
-
-class Test_replace_spelled_num_with_dig(unittest.TestCase):
-
-    def test_replace_spelled_num_with_dig_one_dig(self):
-        expected = "5asdv"
-        input = "fiveasdv"
-        out = replace_first_and_last_spelled_num_with_dig(input)
-        self.assertEqual(expected, out)
-
-    def test_replace_spelled_num_with_dig_no_dig(self):
-        expected = "xrezde"
-        input = "xrezde"
-        out = replace_first_and_last_spelled_num_with_dig(input)
-        self.assertEqual(expected, out)
-
-    def test_replace_spelled_num_with_dig_overlapping_dig(self):
-        expected = "xre8wo3zde"
-        input = "xreeightwothreezde"
-        out = replace_first_and_last_spelled_num_with_dig(input)
-        self.assertEqual(expected, out)
 
 
 
@@ -145,7 +73,19 @@ class Test_find_last_value(unittest.TestCase):
         out = find_last_value(input, values)
         self.assertEqual(expected, out)
 
+class Test_transform_to_digit(unittest.TestCase):
 
+    def test_transform_to_digit(self):
+        input = "one"
+        expected = "1"
+        out = transform_to_digit(input)
+        self.assertEqual(expected, out)
+
+    def test_transform_to_digit_wrong_value(self):
+        input = "wrong_val"
+        expected = None
+        out = transform_to_digit(input)
+        self.assertEqual(expected, out)
 
 
 if __name__ == "__main__":
