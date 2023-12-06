5d6aeb21edf4509078da1e72bfefac1b7ad0ec27
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-06 19:51:14 +0100

    Tests fixed

--- 2023/Day 3/tests.py
+++ 2023/Day 3/tests.py
@@ -111,51 +111,45 @@ class Test_Board_calculate_borders(unittest.TestCase):
 
 class Test_FieldGroupNum_validate_cords(unittest.TestCase):
 
-    def setUp(self):
-        self.g = FieldGroup()
-
     def test_FieldGroup_validate_cords_xcords_positive(self):
         x_cords = [9, 10, 11]
-        self.assertEqual(self.g._validate_x_cords(x_cords), True)
+        self.assertEqual(FieldGroup._validate_x_cords(x_cords), True)
 
 
     def test_FieldGroup_validate_cords_xcords_negative(self):
         x_cords = [9, 10, 12]
-        self.assertEqual(self.g._validate_x_cords(x_cords), False)
+        self.assertEqual(FieldGroup._validate_x_cords(x_cords), False)
 
 
     def test_FieldGroupN_validate_cords_ycords_positive(self):
         y_cords = [9, 9, 9]
-        self.assertEqual(self.g._validate_y_cords(y_cords), True)
+        self.assertEqual(FieldGroup._validate_y_cords(y_cords), True)
 
 
     def test_FieldGroup_validate_cords_ycords_negative(self):
         y_cords = [9, 10, 15]
-        self.assertEqual(self.g._validate_y_cords(y_cords), False)
+        self.assertEqual(FieldGroup._validate_y_cords(y_cords), False)
 
 
 class Test_FieldGroupNum_create_num(unittest.TestCase):
 
     def test_FieldGroupNum_create_num_sorted_in_input(self):
-        gn = FieldGroupNum(Field(1, 2, "5"), Field(1, 3, "6"))
+        gn = FieldGroupNum(Field(2, 1, "5"), Field(3, 1, "6"))
         self.assertEqual(gn.create_num(), 56)
 
 
     def test_FieldGroupNum_create_num_not_sorted_in_input(self):
-        gn = FieldGroupNum(Field(1,3, "5"), Field(1, 2, "6"))
-        self.assertEqual(gn.create_num(), 56)
+        gn = FieldGroupNum(Field(3, 1, "5"), Field(2, 1, "6"))
+        self.assertEqual(gn.create_num(), 65)
 
 
 class Test_FieldGroupNum_validate_values(unittest.TestCase):
 
-    def setUp(self):
-        self.gn = FieldGroupNum()
-
     def test_FieldGroupNum_validate_values_positive(self):
         values = ["9", "1", "5"]
-        self.assertEqual(self.gn._validate_values(values), True)
+        self.assertEqual(FieldGroupNum._validate_values(values), True)
 
 
     def test_FieldGroupNum_validate_values_negative(self):
         values = ["9", "g", "5"]
-        self.assertEqual(self.gn._validate_values(values), False)
+        self.assertEqual(FieldGroupNum._validate_values(values), False)
