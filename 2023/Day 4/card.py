21c5c41634ce45603e10d2efc4dce0785662642b
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-08 22:08:35 +0100

    Refactor

--- 2023/Day 4/card.py
+++ 2023/Day 4/card.py
@@ -1,12 +1,13 @@
 class Card:
-    def __init__(self, card_nums, winning_nums):
+    def __init__(self, card_nums, winning_nums, id=None):
+        self.id = id
         self.card_nums = card_nums
         self.winning_nums = winning_nums
+        self.common_nums_count = len(list(set(self.card_nums).intersection(self.winning_nums)))
         self.points = self._calculate_card_points()
 
     def _calculate_card_points(self):
-        common_nums_count = len(list(set(self.card_nums).intersection(self.winning_nums)))
-        if common_nums_count:
-            return pow(2, common_nums_count - 1)
+        if self.common_nums_count:
+            return pow(2, self.common_nums_count - 1)
         else:
             return 0
--- 2023/Day 4/main.py
+++ 2023/Day 4/main.py
@@ -1,8 +1,19 @@
 from card import Card
 
 with open("input", "r") as f:
+    cards = []
     while line := f.readline().strip():
-        striped_line = line.split(":")[1]
-        winning_nums, card_nums = striped_line.split("|")
-        winning_nums_transofmred = [int(num) for num in winning_nums.split(" ") if num.isdigit()]
-        card_nums_transofmred = [int(num) for num in card_nums.split(" ") if num.isdigit()]
+        splited_line = line.split(":")
+        id = int(splited_line[0].replace("Card", "").strip())
+        winning_nums, card_nums = splited_line[1].split("|")
+        winning_nums_transformed = [int(num) for num in winning_nums.split(" ") if num.isdigit()]
+        card_nums_transformed = [int(num) for num in card_nums.split(" ") if num.isdigit()]
+        card = Card(card_nums_transformed, winning_nums_transformed, id)
+        cards.append(card)
+
+    print(f"Result part 1: {sum([card.points for card in cards])}")
+
+
+cards_deck_with_copies = []
+for c in cards:
+    print(c.common_nums_count)
