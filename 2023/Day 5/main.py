3df3c4885bc7e8ad50dc8f9a426e3a64cc0076fa
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-10 23:00:04 +0100

    Fixed lines reading

--- 2023/Day 5/main.py
+++ 2023/Day 5/main.py
@@ -13,20 +13,12 @@ if __name__ == "__main__":
     values = []
     name = None
     with open("input", "r") as f:
-        file_splited = [l for l in f.read().split("\n") if l != '']
-        for line in file_splited:
-            print(mapps_dict_values)
-            if line.startswith("seeds"):
-                seeds = read_seeds(line)
-            elif line.endswith("map:"):
-                if name is not None:
-                    print(name)
-                    mapps_dict_values[name] = values
-                    values = []
-                name = read_map_name(line)
-            else:
-                values.append(line)
-            mapps_dict_values[name] = values
+        file_splited = [l for l in f.read().split("\n\n") if l != '']
+        seeds = file_splited[0]
+        for mapping in file_splited[1:]:
+            splited_mapping = mapping.split("\n")
+            name = read_map_name(splited_mapping[0])
+            mapps_dict_values[name] = splited_mapping[1:]
 
     mapps_dict_mappings = {}
     for key,values in mapps_dict_values.items():
@@ -36,12 +28,14 @@ if __name__ == "__main__":
             mapp_confs.append(MappingConf(*splited_values))
             mapps_dict_mappings[key] = Mapper(mapp_confs)
 
-
-    # # for s in seeds:
-    #     for k,v in
-    # #     soil = mapps_dict_mappings['seed-to-soil'].map(s)
-    # #     fertilizer = mapps_dict_mappings['soil-to-fertilizer'].map(soil)
-    # #     water = mapps_dict_mappings['fertilizer-to-water'].map(fertilizer)
-    # #     light = mapps_dict_mappings['water-to-light'].map(water)
-    # #     temperature = mapps_dict_mappings['light-to-temperature'].map(light)
-    # #     humidity = mapps_dict_mappings['temperature-to-humidity'].map(temperature)
+    locs = []
+    for s in seeds:
+        soil = mapps_dict_mappings['seed-to-soil'].map(s)
+        fertilizer = mapps_dict_mappings['soil-to-fertilizer'].map(soil)
+        water = mapps_dict_mappings['fertilizer-to-water'].map(fertilizer)
+        light = mapps_dict_mappings['water-to-light'].map(water)
+        temperature = mapps_dict_mappings['light-to-temperature'].map(light)
+        humidity = mapps_dict_mappings['temperature-to-humidity'].map(temperature)
+        location = mapps_dict_mappings['humidity-to-location'].map(humidity)
+        location.append(locs)
+    print(min(locs))
