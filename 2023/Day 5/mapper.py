3ddeae72708e64fa7d6e3d44561071b149bedc09
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-10 21:14:40 +0100

    Change mapper to operate on indexes

--- 2023/Day 5/mapper.py
+++ 2023/Day 5/mapper.py
@@ -2,6 +2,7 @@ from typing import List, Optional
 
 from dataclasses import dataclass
 
+
 @dataclass
 class MappingConf:
     destination_start: int
@@ -14,9 +15,8 @@ class Mapper:
         self._mapping_list_source = []
         self._mapping_list_desc = []
         for mapping in mappings:
-            self._mapping_list_source += self.calculate_range_nums(mapping.source_start, mapping.length)
-            self._mapping_list_desc += self.calculate_range_nums(mapping.destination_start, mapping.length)
-
+            self._mapping_list_source.append(self.calculate_range_nums(mapping.source_start, mapping.length))
+            self._mapping_list_desc.append(self.calculate_range_nums(mapping.destination_start, mapping.length))
 
     @property
     def mapping_list_source(self):
@@ -26,21 +26,24 @@ class Mapper:
     def mapping_list_desc(self):
         return self._mapping_list_desc
 
-
     @staticmethod
     def calculate_range_nums(start: int, length: int):
-        return list(range(start, start+length))
-
+        return range(start, start + length)
 
     def map(self, source):
-        index = self.find_el(self._mapping_list_source, source)
-        if index is None:
+        ranges = [source in r for r in self._mapping_list_source]
+        if any(ranges):
+            r_index = ranges.index(True)
+            v_index = self.find_el(self._mapping_list_source[r_index], source)
+            return self._mapping_list_desc[r_index][v_index]
+        else:
             return source
-        return self._mapping_list_desc[index]
-
 
     @staticmethod
-    def find_el(array: list, value: int) -> Optional[int]:
+    def find_el(array, value) -> Optional[int]:
         if value not in array:
             return None
         return array.index(value)
+
+
+
--- 2023/Day 5/tests.py
+++ 2023/Day 5/tests.py
@@ -1,12 +1,13 @@
 import unittest
 
 from mapper import Mapper, MappingConf
+from main import read_seeds, read_map_name
 
 class Test_Mapper_calculate_range_nums(unittest.TestCase):
     def test_Mapper_calculate_range_nums(self):
         range_start = 98
         len = 2
-        expected = [98,99]
+        expected = range(98, 100)
         output = Mapper.calculate_range_nums(range_start,len)
         self.assertEqual(output, expected)
 
@@ -16,27 +17,34 @@ class Test_Mapper_init_mappings_lists(unittest.TestCase):
         configs = [MappingConf(50, 98, 2), MappingConf(54, 100, 2)]
         self.m = Mapper(configs)
     def test_Mapper_init_mappings_list_sources(self):
-        expected = [98, 99, 100, 101]
+        expected = [range(98, 100), range(100, 102)]
         self.assertEqual(self.m.mapping_list_source, expected)
 
 
     def test_Mapper_init_mappings_list_descs(self):
-        expected = [50, 51, 54, 55]
+        expected = [range(50, 52), range(54, 56)]
         self.assertEqual(self.m.mapping_list_desc, expected)
 
 
 class Test_Mapper_find_el(unittest.TestCase):
     def test_Mapper_find_el_existing_in_array(self):
-        array = [0, 1, 2, 4]
+        array = range(0,5)
         value = 0
         expected = 0
         self.assertEqual(Mapper.find_el(array,value), expected)
 
 
     def test_Mapper_find_el_not_existing_in_array(self):
-        array = [0, 1, 2, 4]
+        array = range(0,3)
         value = 70
         expected = None
+        self.assertEqual(Mapper.find_el(array, value), expected)
+
+
+    def test_Mapper_find_el_list_of_ranges(self):
+        array = [range(0,3), range(5,6)]
+        value = range(5,6)
+        expected = 1
         self.assertEqual(Mapper.find_el(array, value), expected)
 
 
@@ -54,3 +62,18 @@ class Test_Mapper_map_elements(unittest.TestCase):
         v = 4
         expected = 4
         self.assertEqual(self.m.map(v), expected)
+
+
+class Test_read_seed(unittest.TestCase):
+    def test_read_seeds(self):
+        l = "seeds: 92 46 24 21 37"
+        expected = [92, 46, 24, 21, 37]
+        self.assertEqual(read_seeds(l), expected)
+
+
+class Test_read_map_name(unittest.TestCase):
+    def test_read_map_name(self):
+        l = "seed-to-soil map:"
+        expected = "seed-to-soil"
+        self.assertEqual(read_map_name(l), expected)
+
