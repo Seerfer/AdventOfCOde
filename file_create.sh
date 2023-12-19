2c06dd8a718452c11ca77bd1a4a7958e0b9cc3fd
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-19 18:38:06 +0100

    Fix tests file content

--- file_create.sh
+++ file_create.sh
@@ -45,7 +45,7 @@ else
   echo 'import unittest
 
 
-class Test_Mapper_calculate_range_nums(unittest.TestCase):
+class Test_Example_test(unittest.TestCase):
   def test_example_test(self):
     self.assertEqual(2+2, 4)' > "$Year/Day $Day/tests.py"
 fi
