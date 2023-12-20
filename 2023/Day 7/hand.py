89f07570c4c6eb27611c471b5dd15651d6c3c714
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-20 22:26:34 +0100

    Implement card score calculating

--- 2023/Day 7/hand.py
+++ 2023/Day 7/hand.py
@@ -2,8 +2,27 @@ from collections import Counter
 
 
 class Hand:
-    def __init__(self, hand: str):
+    def __init__(self, hand: str, bid:int):
+        self.bid = bid
         self.hand = list(hand)
+        self._score = self.calculate_calculate_score()
+
+    def calculate_calculate_score(self):
+        order = [
+            self.check_five_of_kind,
+            self.check_four_of_kind,
+            self.check_full_house,
+            self.check_three_of_kind,
+            self.check_two_pair,
+            self.check_one_pair
+        ]
+        i = 6
+        for func in order:
+            if func(self.hand):
+                return i
+            else:
+                i -= 1
+        return i
 
     @staticmethod
     def check_five_of_kind(hand: list) -> bool:
