5c09d530e067d5d62f176767131d482434843472
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-20 21:40:00 +0100

    Added day 7 files

---
+++ 2023/Day 7/main.py
@@ -0,0 +1,4 @@
+
+
+if __name__ == "__main__":
+  with open("input", "r") as f:
---
+++ 2023/Day 7/tests.py
@@ -0,0 +1,6 @@
+import unittest
+
+
+class Test_Example_test(unittest.TestCase):
+  def test_example_test(self):
+    self.assertEqual(2+2, 4)
