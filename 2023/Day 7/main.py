b4e93b9ec31659651625c6f4c7448eeec6ec6260
Patryk Ostrowski <patrykost1998@gmail.com>
2023-12-21 00:00:02 +0100

    Part 2 finished

--- 2023/Day 7/main.py
+++ 2023/Day 7/main.py
@@ -1,5 +1,13 @@
-
+from hand import Hand
 
 if __name__ == "__main__":
+  hands = []
   with open("input", "r") as f:
-    pass
+    while line := f.readline().strip():
+      cards, bid = line.split(" ")
+      hands.append(Hand(cards, int(bid)))
+
+  res1 = sum([i*h.bid for i,h in enumerate(sorted(hands), 1)])
+  print(f"Result for part 1: {res1}")
+
+
